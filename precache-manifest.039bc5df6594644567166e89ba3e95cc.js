d69611bc3af306b2def76b4177ae324d91e6e769
Mike Thomson (Intelli-Flex) <mike@iflex.com.au>
2019-05-24 13:32:16 +1000

    Updates

---
+++ precache-manifest.039bc5df6594644567166e89ba3e95cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dc38b7bb9decb35722a133f8bea4323",
+    "url": "/hiresystem/index.html"
+  },
+  {
+    "revision": "4d88759b602ecef8b431",
+    "url": "/hiresystem/static/css/main.578da264.chunk.css"
+  },
+  {
+    "revision": "43a28cdfa699224f3879",
+    "url": "/hiresystem/static/js/2.561bca38.chunk.js"
+  },
+  {
+    "revision": "4d88759b602ecef8b431",
+    "url": "/hiresystem/static/js/main.07c1ac0b.chunk.js"
+  },
+  {
+    "revision": "5eef80cd87d25bd8c2b4",
+    "url": "/hiresystem/static/js/runtime~main.4a10240e.js"
+  }
+]);
